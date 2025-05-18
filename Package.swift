4322f726460c3529d11ca0e9d55d6491b9bef56c
Sven A. Schmidt <sas@finestructure.co>
2025-05-18 13:55:19 +0200

    Executable "plotty"

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "Plotty",
     platforms: [.macOS(.v13)],
+    products: [
+        .executable(name: "plotty", targets: ["Plotty"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
